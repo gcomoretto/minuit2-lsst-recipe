51230b3cc3ebcc514a94691e26a48e47d7b17ef6
Gabriele Comoretto <gcomoretto@lsst.org>
2018-10-30 12:59:19 -0700

    Added recipe

---
+++ recipe/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x -e
+
+INCLUDE_PATH="${PREFIX}/include"
+LIBRARY_PATH="${PREFIX}/lib $LIBRARY_PATH"
+
+if [ "$(uname)" == "Darwin" ]; then
+    TOOLSET=clang
+    CC=clang
+    CXX=clang++
+elif [ "$(uname)" == "Linux" ]; then
+    TOOLSET=gcc
+fi
+
+export CXXFLAGS="${CXXFLAGS} -fPIC -std=c++14 -D_GLIBCXX_USE_CXX11_ABI=0"
+
+./configure --prefix=${PREFIX}
+
+make
+make install
